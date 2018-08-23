4430a337974b09e9f7bcede11c9ee3da43f42756
jraleman <jraleman@bendiburg.com>
2018-08-22 23:32:57 -0700

    Fix Gruntfile uncss task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,37 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'public/**/*.js'],
-      options: {
-        globals: {
-          jQuery: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
-    },
+    // jshint: {
+    //   files: ['Gruntfile.js', 'src/**/*.js', 'public/**/*.js'],
+    //   options: {
+    //     globals: {
+    //       jQuery: true
+    //     }
+    //   }
+    // },
+    // watch: {
+    //   files: ['<%= jshint.files %>'],
+    //   tasks: ['jshint']
+    // },
     uncss: {
       dist: {
         src: ['public/index.html'],
-        dest: 'public/css/styles.css'
+        dest: 'dist/cleaned.css',
         options: {
           report: 'min' // optional: include to report savings
         }
       }
-    }
+    },
     processhtml: {
       dist: {
         files: {
-          'new_index.html': ['public/index.html']
+          'dist/cleaned.css': ['public/index.html']
         }
       }
+    }
   });
-  grunt.registerTask('default', ['jshint']);
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  // grunt.registerTask('default', ['jshint']);
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['uncss']);
   grunt.loadNpmTasks('grunt-uncss');
 };
