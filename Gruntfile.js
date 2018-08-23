e4afb6b3be680b8820f8179d5b28204cf0835034
jraleman <jraleman@bendiburg.com>
2018-08-22 23:11:52 -0700

    Link uncss Grunt task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'public/**/*.js'],
@@ -12,12 +11,25 @@ module.exports = function(grunt) {
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
+    },
+    uncss: {
+      dist: {
+        src: ['public/index.html'],
+        dest: 'public/css/styles.css'
+        options: {
+          report: 'min' // optional: include to report savings
+        }
+      }
     }
+    processhtml: {
+      dist: {
+        files: {
+          'new_index.html': ['public/index.html']
+        }
+      }
   });
-
+  grunt.registerTask('default', ['jshint']);
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', ['jshint']);
-
+  grunt.loadNpmTasks('grunt-uncss');
 };
