fba87cda3ce9a2594a61722a6fae1be8a5df0f33
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-08-25 21:26:08 -0700

    Set GridContainer states

--- src/containers/GridContainer/GridContainer.js
+++ src/containers/GridContainer/GridContainer.js
@@ -3,7 +3,12 @@ import React from 'react';
 class GridContainer extends React.Component {
   constructor (prop) {
     super(props);
-    this.state = {};
+    this.state = {
+      appsCount: appsList.length,
+        toggle: []
+    };
+    // Creates N amount of 'bool' elements inside the toggle array
+    this.state.toggle = appsList.map(() => false);
     return ;
   }
   render () {
