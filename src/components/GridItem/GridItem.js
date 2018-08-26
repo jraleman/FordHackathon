c1515c12ae45598c268aaee89d8b968366a37073
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-08-25 23:24:53 -0700

    Update GridItem render function

--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -3,9 +3,12 @@ import React from 'react';
 class GridItem extends React.Component {
   constructor (props) {
     super(props);
-    this._appTitle = this.props.title;
+    this._appTitleStron = this.props.titleStrong;
+    this._appTitleLight = this.props.titleLight;
+    this._appTitle = this.props.titleStrong + this.props.titleLight;
     this._appIcon = this.props.icon;
     this._appDescription = this.props.desc;
+    this._appPreview = this.props.preview;
     this._termOfServices = this.props.tos;
     this._privacyPolicy = this.props.pc;
     this._onPress = this.props.onPress;
@@ -14,9 +17,40 @@ class GridItem extends React.Component {
   }
   render () {
     return (
-      <div>
-        <p>{ "GridItem Component" }</p>
-      </div>
+      <React.Fragment>
+        <div className="cbp-item col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 category1">
+          <div className="cbp-caption">
+            <div className="cbp-caption-hover-gradient">
+              <img
+                src={ this._appIcon }
+                alt={ this._appTitle }
+              />
+            </div>
+            <div className="cbp-caption-activeWrap">
+              <div className="cbp-l-caption-alignCenter">
+                <div className="cbp-l-caption-body portfolio-icons">
+                  <a
+                    href={ this._appPreview }
+                    className="cbp-lightbox"
+                    { }/* Check the line below, maybe it can help me fix nostromo... { data-title }, camelCase?  */ }
+                    data-title="ItemOne"
+                  >
+                    <i className="fa fa-info"></i>
+                  </a>
+                  { /* Open modal here... update MetroCube template to add Modal */ }
+                  <a href="#">
+                    <i className="fa fa-download"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h4 className="cbp-title">
+            <strong>{ this._appTitleStrong }</strong>
+            { this._appTitleLight }
+          </h4>
+        </div>
+      </React.Fragment>
     );
   }
 }
