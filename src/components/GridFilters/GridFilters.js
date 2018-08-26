856b5ea26b56e99021302e5df2370a532366a5c5
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-08-26 05:19:19 -0700

    Add dumnmy GridFilters component

---
+++ src/components/GridFilters/GridFilters.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class GridFilters extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    return ;
+  }
+  render () {
+    return (
+      <React.Fragment>
+        <p>{ "GridFilters" }</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default GridFilters;
