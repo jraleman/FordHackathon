533deee894b9eec360041aea4d1ae00c20038d24
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-08-26 05:19:45 -0700

    Add dumnmy GridTitle component

---
+++ src/components/GridTitle/GridTItle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class GridTitle extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    return ;
+  }
+  render () {
+    return (
+      <React.Fragment>
+        <p>{ "GridTitle" }</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default GridTitle;
