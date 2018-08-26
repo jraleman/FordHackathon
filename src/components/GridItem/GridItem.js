371f31fd6f52f8d1a640eefc8f522783f172f4a8
Jose Ramon Aleman <jraleman@bendiburg.com>
2018-08-25 23:10:04 -0700

    Set GridItems props

--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -3,6 +3,12 @@ import React from 'react';
 class GridItem extends React.Component {
   constructor (props) {
     super(props);
+    this._appTitle = this.props.title;
+    this._appIcon = this.props.icon;
+    this._appDescription = this.props.desc;
+    this._termOfServices = this.props.tos;
+    this._privacyPolicy = this.props.pc;
+    this._onPress = this.props.onPress;
     this.state = {};
     return ;
   }
