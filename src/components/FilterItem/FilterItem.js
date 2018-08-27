7465a46034e5f363b5bbf57d05f950a3e975e2b0
jraleman <jraleman@bendiburg.com>
2018-08-26 18:06:28 -0700

    Add dummy Filter component and container

---
+++ src/components/FilterItem/FilterItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class FilterItem extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    return ;
+  }
+  render () {
+    return (
+      <React.Fragment>
+        <p>{ "FilterItem" }</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default FilterItem;
---
+++ src/containers/FilterContainer/FilterContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class FilterContainer extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    return ;
+  }
+  render () {
+    return (
+      <React.Fragment>
+        <p>{ "FilterContainer" }</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default FilterContainer;
